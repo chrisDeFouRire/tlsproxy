4863fc282d23bf4e78ddba79c267b219d5ad769c
chris <chris@xtralife.cloud>
2017-07-20 16:33:42 +0100

    debugging

--- main.go
+++ main.go
@@ -82,13 +82,15 @@ func main() {
 		u, _ := url.Parse(*backend)
 
 		director := func(req *http.Request) {
-			req.URL.Scheme = u.Scheme
-			//req.URL.Host = u.Host
+			req.URL.Scheme = "http://"
+			req.URL.Host = req.Header.Get("Host")
 			req.URL.Path = u.Path + "/" + req.URL.Path
 			if _, ok := req.Header["User-Agent"]; !ok {
 				// explicitly disable User-Agent so it's not set to default value
 				req.Header.Set("User-Agent", "")
 			}
+			log.Print(req)
+			log.Print(req.Header)
 		}
 		proxy := &httputil.ReverseProxy{Director: director}
 
